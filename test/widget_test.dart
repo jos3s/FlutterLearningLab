82e1bf0916e81314583ffdf82aafe9ff54b2766e
Jos3s <karatedkgt17@gmail.com>
2022-10-19 21:38:55 -0300

    Atualizando outros arquivos com o nome do pacote

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:teste/app_widget.dart';
+import 'package:Lembretes_Flutterama/app_widget.dart';
 
 
 void main() {
