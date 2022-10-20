a7ca04fc3307f73bf954addb9ffa8615fc587c55
Jos3s <karatedkgt17@gmail.com>
2022-10-20 20:31:49 -0300

    Criando modelo do user

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  final int codigo;
+  String nome;
+  String email;
+  String senha;
+  
+  User({
+    required this.codigo,
+    required this.email,
+    required this.nome,
+    required this.senha,
+  })
+}
